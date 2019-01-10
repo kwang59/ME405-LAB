d13f7400b4f983f74efb29ca74451c0f194fbeec
kwang59 <46506417+kwang59@users.noreply.github.com>
2019-01-10 00:44:54 -0800

    Update main.py
    Charlie tried to be picky

--- main.py
+++ main.py
@@ -16,21 +16,21 @@ class MotorDriver:
         print ('Creating a motor driver')
         pinA10 = pyb.Pin (pyb.Pin.board.PA10, pyb.Pin.OUT_PP) #ENA -> pin10
         pinA10.high()                                         #enable motor
-    def set_duty_cycle (self, level):
-        ''' This method sets the duty cycle to be sent to the motor to the given level. Positive values
-        cause torque in one direction, negative values in the opposite direction.
-        @param level A signed integer holding the duty cycle of the voltage sent to the motor '''
-        import pyb    
         pinB4 = pyb.Pin (pyb.Pin.board.PB4, pyb.Pin.OUT_PP) #set pinb4 to output
         pinB5 = pyb.Pin (pyb.Pin.board.PB5, pyb.Pin.OUT_PP) #set pinb5 to output
         tim3 = pyb.Timer (3, freq = 20000)                  #configure Timer3
-        ch1 = tim3.channel(1, pyb.Timer.PWM, pin = pinB4)   #set channel1 to pwm
-        ch2 = tim3.channel(2, pyb.Timer.PWM, pin = pinB5)   #set channel2 to pwm
+        self.ch1 = tim3.channel(1, pyb.Timer.PWM, pin = pinB4)   #set channel1 to pwm
+        self.ch2 = tim3.channel(2, pyb.Timer.PWM, pin = pinB5)   #set channel2 to pwm
+    def set_duty_cycle (self, level):
+        ''' This method sets the duty cycle to be sent to the motor to the given level. Positive values
+        cause torque in one direction, negative values in the opposite direction.
+        @param level A signed integer holding the duty cycle of the voltage sent to the motor '''  
+
         print ('Setting duty cycle to ' + str (level))        
         if level >= 0:
-            pinB4.high()
-            ch2.pulse_width_percent(level)
+            self.pinB4.high()
+            self.ch2.pulse_width_percent(level)
         else:
-            pinB5.high()
-            ch1.pulse_width_percent(-level)
+            self.pinB5.high()
+            self.ch1.pulse_width_percent(-level)
 
